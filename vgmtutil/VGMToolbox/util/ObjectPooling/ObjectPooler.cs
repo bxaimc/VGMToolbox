8d47e44f374f8904703a74341f5adedf8cfc81b3
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2008-11-16 00:36:12 +0000

    Added a folder remotely

---
+++ vgmtutil/VGMToolbox/util/ObjectPooling/ObjectPooler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VGMToolbox.util.ObjectPooling
+{
+    public sealed class ObjectPooler
+    {
+        static readonly PoolByteArrays instance = new PoolByteArrays(5, 104857600);        
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static ObjectPooler() {}
+
+        ObjectPooler() { }
+
+        public static PoolByteArrays Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
