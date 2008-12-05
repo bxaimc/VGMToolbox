e45a76c31685105ea7845d253be89d4dd4008b1f
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2008-12-05 23:49:56 +0000

    [VGMT] GUI Work.

--- VGMToolbox/Form2.Designer.cs
+++ VGMToolbox/Form2.Designer.cs
@@ -32,20 +32,22 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tvMenuTree = new System.Windows.Forms.TreeView();
             this.pnlNds_SdatExtractor = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripProgressBar2 = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlXsf_2sfRipper = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.pnlNds_SdatExtractor.SuspendLayout();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.pnlXsf_2sfRipper.SuspendLayout();
-            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -87,15 +89,73 @@
             this.pnlNds_SdatExtractor.AutoScroll = true;
             this.pnlNds_SdatExtractor.AutoSize = true;
             this.pnlNds_SdatExtractor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pnlNds_SdatExtractor.Controls.Add(this.textBox1);
             this.pnlNds_SdatExtractor.Controls.Add(this.label1);
-            this.pnlNds_SdatExtractor.Controls.Add(this.label3);
-            this.pnlNds_SdatExtractor.Controls.Add(this.statusStrip1);
+            this.pnlNds_SdatExtractor.Controls.Add(this.textBox1);
+            this.pnlNds_SdatExtractor.Controls.Add(this.toolStripContainer1);
             this.pnlNds_SdatExtractor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlNds_SdatExtractor.Location = new System.Drawing.Point(0, 0);
             this.pnlNds_SdatExtractor.Name = "pnlNds_SdatExtractor";
             this.pnlNds_SdatExtractor.Size = new System.Drawing.Size(537, 396);
             this.pnlNds_SdatExtractor.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 296);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(533, 78);
+            this.textBox1.TabIndex = 3;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.toolStrip1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(533, 18);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 374);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(533, 18);
+            this.toolStripContainer1.TabIndex = 5;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripProgressBar2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(533, 18);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(78, 23);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // toolStripProgressBar2
+            // 
+            this.toolStripProgressBar2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripProgressBar2.Name = "toolStripProgressBar2";
+            this.toolStripProgressBar2.Size = new System.Drawing.Size(350, 23);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sdat";
             // 
             // pnlXsf_2sfRipper
             // 
@@ -115,57 +175,6 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "2sfripper";
             // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1,
-            this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 370);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripProgressBar1
-            // 
-            this.toolStripProgressBar1.AutoSize = false;
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(315, 16);
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.AutoSize = false;
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(200, 17);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
-            // 
-            // label1
-            // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Location = new System.Drawing.Point(3, 355);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // label3
-            // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Location = new System.Drawing.Point(63, 355);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(466, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
-            // 
-            // textBox1
-            // 
-            this.textBox1.AcceptsReturn = true;
-            this.textBox1.Location = new System.Drawing.Point(2, 274);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(528, 78);
-            this.textBox1.TabIndex = 3;
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -175,17 +184,21 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "VGMToolbox v2.0";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
             this.splitContainer1.ResumeLayout(false);
             this.pnlNds_SdatExtractor.ResumeLayout(false);
             this.pnlNds_SdatExtractor.PerformLayout();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.pnlXsf_2sfRipper.ResumeLayout(false);
             this.pnlXsf_2sfRipper.PerformLayout();
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,13 +210,13 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TreeView tvMenuTree;
         private System.Windows.Forms.Panel pnlNds_SdatExtractor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar2;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel pnlXsf_2sfRipper;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- VGMToolbox/Form2.cs
+++ VGMToolbox/Form2.cs
@@ -12,6 +12,8 @@ namespace VGMToolbox
 {
     public partial class Form2 : Form
     {
+        Font treeviewBoldFont;
+        
         public Form2()
         {
             InitializeComponent();
@@ -21,7 +23,11 @@ namespace VGMToolbox
 
         private void buildMenuNodes()
         {
+            this.treeviewBoldFont = new Font(this.tvMenuTree.Font, FontStyle.Bold);
+            
             TreeNode rootNode = new TreeNode("VGMToolbox");
+            rootNode.NodeFont = this.treeviewBoldFont;
+
             Constants.NodeTagStruct nodeTag = new Constants.NodeTagStruct();
 
             ////////////
@@ -31,7 +37,8 @@ namespace VGMToolbox
             TreeNode audting_DatafileCreatorNode = new TreeNode("Datafile Creator");
             TreeNode audting_RebuilderNode = new TreeNode("Rebuilder");
             TreeNode audting_DatafileCheckerNode = new TreeNode("Datafile Checker");
-            
+            audting_RootNode.NodeFont = this.treeviewBoldFont;
+
             audting_RootNode.Nodes.Add(audting_DatafileCreatorNode);
             audting_RootNode.Nodes.Add(audting_RebuilderNode);
             audting_RootNode.Nodes.Add(audting_DatafileCheckerNode);
@@ -44,6 +51,7 @@ namespace VGMToolbox
             TreeNode examine_RootNode = new TreeNode("Examine/Exploration Tools");
             TreeNode examine_TagViewerNode = new TreeNode("Tag/Info Viewer");
             TreeNode examine_MDXCheckerNode = new TreeNode("MDX Checker");
+            examine_RootNode.NodeFont = this.treeviewBoldFont;
 
             examine_RootNode.Nodes.Add(examine_TagViewerNode);
             examine_RootNode.Nodes.Add(examine_MDXCheckerNode);
@@ -54,11 +62,13 @@ namespace VGMToolbox
             // Tools
             /////////
             TreeNode tools_RootNode = new TreeNode("Misc. Tools");
+            tools_RootNode.NodeFont = this.treeviewBoldFont;
 
             // Hoot
             TreeNode hoot_RootNode = new TreeNode("Hoot Tools");
             TreeNode hoot_CsvDatafileNode = new TreeNode("CSV to Datafile");
             TreeNode hoot_XmlBuilderNode = new TreeNode("Xml Builder");
+            hoot_RootNode.NodeFont = this.treeviewBoldFont;
 
             hoot_RootNode.Nodes.Add(hoot_CsvDatafileNode);
             hoot_RootNode.Nodes.Add(hoot_XmlBuilderNode);
@@ -68,6 +78,7 @@ namespace VGMToolbox
             // NSF
             TreeNode nsf_RootNode = new TreeNode("NSF Tools");
             TreeNode nsf_NsfeM3uNode = new TreeNode("NSFE to NSF + M3U");
+            nsf_RootNode.NodeFont = this.treeviewBoldFont;
 
             nsf_RootNode.Nodes.Add(nsf_NsfeM3uNode);
             
@@ -76,6 +87,7 @@ namespace VGMToolbox
             // GBS
             TreeNode gbs_RootNode = new TreeNode("GBS Tools");
             TreeNode gbs_M3uNode = new TreeNode("GBS M3U Creator");
+            gbs_RootNode.NodeFont = this.treeviewBoldFont;
 
             gbs_RootNode.Nodes.Add(gbs_M3uNode);
             
@@ -92,6 +104,8 @@ namespace VGMToolbox
             TreeNode xsf_2sfTimerNode = new TreeNode("2SF Timer");
             TreeNode xsf_MkPsf2FENode = new TreeNode("mkpsf2 Front End");
             TreeNode xsf_UnPsf2FENode = new TreeNode("unpsf2 Front End");
+            xsf_RootNode.NodeFont = this.treeviewBoldFont;
+
 
             xsf_RootNode.Nodes.Add(xsf_xsf2ExeNode);
             xsf_RootNode.Nodes.Add(xsf_2sfRipperNode);
@@ -104,6 +118,7 @@ namespace VGMToolbox
             // NDS
             TreeNode nds_RootNode = new TreeNode("NDS Tools");
             TreeNode nds_SdatExtractorNode = new TreeNode("SDAT Extractor");
+            nds_RootNode.NodeFont = this.treeviewBoldFont;
 
             nodeTag.panel = "pnlNds_SdatExtractor";
             nds_SdatExtractorNode.Tag = nodeTag;
