9eb351f8befde5b0a541d3b58150e065e74361e7
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2009-03-16 01:42:35 +0000

    Rearranged code to put .dlls in subdirs.

--- VGMToolbox/forms/Auditing_CollectionBuilderForm.Designer.cs
+++ VGMToolbox/forms/Auditing_CollectionBuilderForm.Designer.cs
@@ -36,30 +36,30 @@
             // 
             // pnlLabels
             // 
-            this.pnlLabels.Location = new System.Drawing.Point(0, 533);
-            this.pnlLabels.Size = new System.Drawing.Size(722, 19);
+            this.pnlLabels.Location = new System.Drawing.Point(0, 581);
+            this.pnlLabels.Size = new System.Drawing.Size(1262, 19);
             // 
             // pnlTitle
             // 
-            this.pnlTitle.Size = new System.Drawing.Size(722, 20);
+            this.pnlTitle.Size = new System.Drawing.Size(1262, 20);
             // 
             // tbOutput
             // 
-            this.tbOutput.Location = new System.Drawing.Point(0, 456);
-            this.tbOutput.Size = new System.Drawing.Size(722, 77);
+            this.tbOutput.Location = new System.Drawing.Point(0, 504);
+            this.tbOutput.Size = new System.Drawing.Size(1262, 77);
             // 
             // pnlButtons
             // 
-            this.pnlButtons.Location = new System.Drawing.Point(0, 436);
-            this.pnlButtons.Size = new System.Drawing.Size(722, 20);
+            this.pnlButtons.Location = new System.Drawing.Point(0, 484);
+            this.pnlButtons.Size = new System.Drawing.Size(1262, 20);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(662, 0);
+            this.btnCancel.Location = new System.Drawing.Point(1202, 0);
             // 
             // btnDoTask
             // 
-            this.btnDoTask.Location = new System.Drawing.Point(602, 0);
+            this.btnDoTask.Location = new System.Drawing.Point(1142, 0);
             // 
             // comboBox1
             // 
@@ -73,7 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 574);
+            this.ClientSize = new System.Drawing.Size(1262, 622);
             this.Controls.Add(this.comboBox1);
             this.Name = "Auditing_CollectionBuilderForm";
             this.Text = "CollectionBuilderForm";
