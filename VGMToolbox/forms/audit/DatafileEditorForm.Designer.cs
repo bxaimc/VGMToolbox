fdf110707236bf376ea171f0e794644e344061ea
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2009-08-07 17:33:06 +0000

    datafile editor forms

---
+++ VGMToolbox/forms/audit/DatafileEditorForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace VGMToolbox.forms.audit
+{
+    partial class DatafileEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpSource = new System.Windows.Forms.GroupBox();
+            this.browseSourceButton = new System.Windows.Forms.Button();
+            this.datafileSourcePath = new System.Windows.Forms.TextBox();
+            this.treeNodeContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.pnlLabels.SuspendLayout();
+            this.pnlTitle.SuspendLayout();
+            this.pnlButtons.SuspendLayout();
+            this.grpSource.SuspendLayout();
+            this.treeNodeContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeViewTools
+            // 
+            this.treeViewTools.LineColor = System.Drawing.Color.Black;
+            this.treeViewTools.Size = new System.Drawing.Size(857, 218);
+            this.treeViewTools.MouseUp += new System.Windows.Forms.MouseEventHandler(this.treeViewTools_MouseUp);
+            // 
+            // splitContainer1
+            // 
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grpSource);
+            this.splitContainer1.Size = new System.Drawing.Size(857, 394);
+            this.splitContainer1.SplitterDistance = 218;
+            // 
+            // pnlLabels
+            // 
+            this.pnlLabels.Location = new System.Drawing.Point(0, 514);
+            this.pnlLabels.Size = new System.Drawing.Size(857, 19);
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.Size = new System.Drawing.Size(857, 20);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(0, 437);
+            this.tbOutput.Size = new System.Drawing.Size(857, 77);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Location = new System.Drawing.Point(0, 417);
+            this.pnlButtons.Size = new System.Drawing.Size(857, 20);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(797, 0);
+            // 
+            // btnDoTask
+            // 
+            this.btnDoTask.Location = new System.Drawing.Point(737, 0);
+            this.btnDoTask.Click += new System.EventHandler(this.btnDoTask_Click);
+            // 
+            // grpSource
+            // 
+            this.grpSource.Controls.Add(this.browseSourceButton);
+            this.grpSource.Controls.Add(this.datafileSourcePath);
+            this.grpSource.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpSource.Location = new System.Drawing.Point(0, 0);
+            this.grpSource.Name = "grpSource";
+            this.grpSource.Size = new System.Drawing.Size(857, 80);
+            this.grpSource.TabIndex = 0;
+            this.grpSource.TabStop = false;
+            this.grpSource.Text = "Source";
+            // 
+            // browseSourceButton
+            // 
+            this.browseSourceButton.Location = new System.Drawing.Point(282, 19);
+            this.browseSourceButton.Name = "browseSourceButton";
+            this.browseSourceButton.Size = new System.Drawing.Size(25, 20);
+            this.browseSourceButton.TabIndex = 1;
+            this.browseSourceButton.Text = "...";
+            this.browseSourceButton.UseVisualStyleBackColor = true;
+            this.browseSourceButton.Click += new System.EventHandler(this.browseSourceButton_Click);
+            // 
+            // datafileSourcePath
+            // 
+            this.datafileSourcePath.Location = new System.Drawing.Point(6, 19);
+            this.datafileSourcePath.Name = "datafileSourcePath";
+            this.datafileSourcePath.Size = new System.Drawing.Size(270, 20);
+            this.datafileSourcePath.TabIndex = 0;
+            // 
+            // treeNodeContextMenuStrip
+            // 
+            this.treeNodeContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renameToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.treeNodeContextMenuStrip.Name = "treeNodeContextMenuStrip";
+            this.treeNodeContextMenuStrip.Size = new System.Drawing.Size(114, 70);
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.renameToolStripMenuItem.Text = "Rename";
+            this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // DatafileEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 555);
+            this.Name = "DatafileEditorForm";
+            this.Text = "DatafileEditorForm";
+            this.Controls.SetChildIndex(this.pnlLabels, 0);
+            this.Controls.SetChildIndex(this.tbOutput, 0);
+            this.Controls.SetChildIndex(this.pnlTitle, 0);
+            this.Controls.SetChildIndex(this.pnlButtons, 0);
+            this.Controls.SetChildIndex(this.splitContainer1, 0);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.pnlLabels.ResumeLayout(false);
+            this.pnlLabels.PerformLayout();
+            this.pnlTitle.ResumeLayout(false);
+            this.pnlTitle.PerformLayout();
+            this.pnlButtons.ResumeLayout(false);
+            this.pnlButtons.PerformLayout();
+            this.grpSource.ResumeLayout(false);
+            this.grpSource.PerformLayout();
+            this.treeNodeContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpSource;
+        private System.Windows.Forms.Button browseSourceButton;
+        private System.Windows.Forms.TextBox datafileSourcePath;
+        private System.Windows.Forms.ContextMenuStrip treeNodeContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
---
+++ VGMToolbox/forms/audit/DatafileEditorForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+using VGMToolbox.auditing;
+using VGMToolbox.format.auditing;
+using VGMToolbox.plugin;
+
+namespace VGMToolbox.forms.audit
+{
+    public partial class DatafileEditorForm : TreeViewVgmtForm
+    {
+        private datafile datafileObject;
+
+        public DatafileEditorForm(TreeNode pTreeNode) : 
+            base(pTreeNode)
+        {
+            InitializeComponent();
+
+            btnDoTask.Text = "Save As";
+        }
+
+        /// <summary>
+        /// Browse to the source file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void browseSourceButton_Click(object sender, EventArgs e)
+        {
+            this.datafileSourcePath.Text = base.browseForFile(sender, e);
+
+            if (base.checkFileExists(this.datafileSourcePath.Text, this.grpSource.Text))
+            {
+                try
+                {
+                    this.datafileObject = this.getDataFileObject();
+                    this.loadDataFileIntoTree(this.datafileObject);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(String.Format("Error parsing data file: {0}{1}", ex.Message, Environment.NewLine), "Error");
+                }
+            }
+        }
+
+        private datafile getDataFileObject()
+        {
+            datafile datafileObject = new datafile();
+            XmlSerializer serializer = new XmlSerializer(typeof(datafile));
+            
+            using (FileStream xmlFs = File.OpenRead(this.datafileSourcePath.Text))
+            {
+                using (XmlTextReader textReader = new XmlTextReader(xmlFs))
+                {
+                    datafileObject = (datafile)serializer.Deserialize(textReader);
+                }
+            }
+
+            return datafileObject;
+        }
+
+        private void loadDataFileIntoTree(datafile datafileObject)
+        {
+            TreeNode workingGameNode;
+            TreeNode workingRomNode;
+
+            // clear tree
+            this.treeViewTools.Nodes.Clear();
+
+            foreach (game g in datafileObject.game)
+            {
+                workingGameNode = new TreeNode(g.name);
+                workingGameNode.Tag = g.DeepCopy();
+
+                foreach (rom r in g.rom)
+                {
+                    workingRomNode = new TreeNode(r.name);
+                    workingRomNode.Tag = r.DeepCopy();
+
+                    workingGameNode.Nodes.Add(workingRomNode);
+                }
+
+                this.treeViewTools.Nodes.Add(workingGameNode);
+            }
+        }
+
+        protected override void treeViewTools_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.selectedNode = treeViewTools.GetNodeAt(e.X, e.Y);
+                this.oldNode = treeViewTools.SelectedNode;
+                treeViewTools.SelectedNode = this.selectedNode;
+                treeViewTools.LabelEdit = true;
+
+                if (this.selectedNode != null &&
+                    ((this.selectedNode.Tag is game) || (this.selectedNode.Tag is rom)))
+                {
+                    this.treeNodeContextMenuStrip.Show(treeViewTools, e.X, e.Y);
+                }
+            }
+        }
+
+        private void renameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectedNode != null)
+            {
+                treeViewTools.SelectedNode = selectedNode;
+                treeViewTools.LabelEdit = true;
+                if (!selectedNode.IsEditing)
+                {
+                    selectedNode.BeginEdit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No tree node selected or selected node.", "Invalid selection");
+            }
+
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (selectedNode != null)
+            {
+                treeViewTools.SelectedNode = selectedNode;
+
+                game sourceGame = null;
+                rom sourceRom = null;
+
+                if (selectedNode.Tag is rom)
+                {
+                    sourceRom = (rom)selectedNode.Tag;
+                    sourceGame = (game)selectedNode.Parent.Tag;
+                }
+                else
+                {
+                    sourceGame = (game)selectedNode.Tag;
+                }
+
+                this.datafileObject = AuditingUtil.DeleteItemFromDatafile(this.datafileObject, sourceGame, sourceRom);
+                this.loadDataFileIntoTree(this.datafileObject);
+            }
+            else
+            {
+                MessageBox.Show("No tree node selected or selected node.", "Invalid selection");
+            }
+        }
+
+        private void btnDoTask_Click(object sender, EventArgs e)
+        {
+            string saveFilePath = base.browseForFileToSave(sender, e);
+
+            if (!String.IsNullOrEmpty(saveFilePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(this.datafileObject.GetType());
+
+                using (TextWriter textWriter = new StreamWriter(saveFilePath))
+                {
+                    serializer.Serialize(textWriter, this.datafileObject);
+
+                }
+            }
+        }
+
+        
+
+
+    }
+}
