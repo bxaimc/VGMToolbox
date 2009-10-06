9554c7e4b98fa961a986d723ba6c624f133d2270
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2009-10-06 03:04:18 +0000

    [GZIPEXT] Add gzipext.exe to project.

---
+++ gzipext/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using VGMToolbox.util;
+
+namespace gzipext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inFilename;
+            string outFilename;
+            string startOffset;
+
+            string fullInputPath;
+            string fullOutputPath;
+
+            long longStartOffset;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: gzipext.exe <infile> <outfile> <start offset>");
+                Console.WriteLine("   or: gzipext.exe <infile> <outfile>");
+                Console.WriteLine();
+                Console.WriteLine("The 2 parameter option will set <start offset> to 0.");
+            }
+            else
+            {
+                inFilename = args[0];
+                outFilename = args[1];
+
+                if (args.Length < 3)
+                {
+                    startOffset = "0";
+                }
+                else
+                {
+                    startOffset = args[2];
+                }
+
+                fullInputPath = Path.GetFullPath(inFilename);
+                fullOutputPath = Path.GetFullPath(outFilename);
+
+                if (File.Exists(fullInputPath))
+                {
+                    using (FileStream fs = File.OpenRead(fullInputPath))
+                    {
+                        if (startOffset.StartsWith("0x"))
+                        {
+                            startOffset = startOffset.Substring(2);
+                            longStartOffset = long.Parse(startOffset, System.Globalization.NumberStyles.HexNumber, null);
+                        }
+                        else
+                        {
+                            longStartOffset = long.Parse(startOffset, System.Globalization.NumberStyles.Integer, null);
+                        }
+
+                        if (longStartOffset > fs.Length)
+                        {
+                            Console.WriteLine(String.Format("Sorry that start offset is larger than the entire file: {0}", fs.Length.ToString()));
+                        }
+                        else
+                        {
+                            CompressionUtil.DecompressGzipStreamToFile(fs, fullOutputPath, longStartOffset);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File Not Found: " + fullInputPath);
+                }
+            }
+        }
+    }
+}
