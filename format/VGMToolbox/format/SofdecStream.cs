50fe72381d5c04453eb70b1e044e8ce6e78937a4
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2011-02-26 15:10:22 +0000

    [MPEG DEMUX] Add file type detection for SFD.

--- format/VGMToolbox/format/SofdecStream.cs
+++ format/VGMToolbox/format/SofdecStream.cs
@@ -1,17 +1,46 @@
 ﻿using System;
 using System.IO;
+using VGMToolbox.util;
 
 namespace VGMToolbox.format
 {
     public class SofdecStream : Mpeg1Stream
-    {
-        new public const string DefaultAudioExtension = ".adx";
+    {        
         new public const string DefaultVideoExtension = ".m2v";
+
+        new public const string AdxAudioExtension = ".adx";
+        public const string AixAudioExtension = ".aix";
+
+        public static readonly byte[] AixSignatureBytes = new byte[] { 0x41, 0x49, 0x58, 0x46 };
 
         public SofdecStream(string path): base(path) 
         {
-            this.FileExtensionAudio = DefaultAudioExtension;
+            this.FileExtensionAudio = AdxAudioExtension;
             this.FileExtensionVideo = DefaultVideoExtension;          
+        }
+
+        protected override string GetAudioFileExtension(Stream readStream, long currentOffset)
+        {
+            string fileExtension;
+            byte[] checkBytes;
+
+            int headerSize = this.GetAudioPacketHeaderSize(readStream, currentOffset);
+            checkBytes = ParseFile.ParseSimpleOffset(readStream, (currentOffset + 6 + headerSize), 4);
+
+            if (ParseFile.CompareSegment(checkBytes, 0, AixSignatureBytes))
+            {
+                fileExtension = AixAudioExtension;
+            }
+            else if (checkBytes[0] == 0x80)
+            {
+                fileExtension = AdxAudioExtension;
+            }
+            else
+            {
+                fileExtension = ".bin";
+            }
+
+            return fileExtension;
         }
     }
 }
