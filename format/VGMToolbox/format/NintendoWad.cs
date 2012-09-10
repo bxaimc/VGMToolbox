93fb248df6e2fc8bfef2d30b2335f739da76928f
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2012-09-10 03:43:27 +0000

    [WII WAD UNPACKER] Adding WAD Unpacker.

--- format/VGMToolbox/format/NintendoWad.cs
+++ format/VGMToolbox/format/NintendoWad.cs
@@ -21,12 +21,16 @@ namespace VGMToolbox.format
         public uint CertificateChainSize { set; get; }
         public uint Reserved { set; get; }
         public uint TicketSize { set; get; }
-        public uint TmdSize { set; get; }
+        public uint TitleMetaDataSize { set; get; }
         public uint DataSize { set; get; }
         public uint FooterSize { set; get; }
         
-        // CERT
+        // offsets
         public uint CertificateChainOffset { set; get; }
+        public uint TicketOffset { set; get; }
+        public uint TitleMetaDataOffset { set; get; }
+        public uint DataOffset { set; get; }
+        public uint FooterOffset { set; get; }
 
         // TITLE
         public ulong TitleId { set; get; }
@@ -48,12 +52,17 @@ namespace VGMToolbox.format
                     this.CertificateChainSize = ParseFile.ReadUintBE(fs, 8);
                     this.Reserved = ParseFile.ReadUintBE(fs, 0xC);
                     this.TicketSize = ParseFile.ReadUintBE(fs, 0x10);
-                    this.TmdSize = ParseFile.ReadUintBE(fs, 0x14);
+                    this.TitleMetaDataSize = ParseFile.ReadUintBE(fs, 0x14);
                     this.DataSize = ParseFile.ReadUintBE(fs, 0x18);
                     this.FooterSize = ParseFile.ReadUintBE(fs, 0x1C);
                     
-                    // CERT
+                    // offsets
                     this.CertificateChainOffset = this.PadValue(this.HeaderSize, "Certificate Chain Offset");
+                    this.TicketOffset = this.PadValue(this.CertificateChainOffset + this.CertificateChainSize, "Ticket Offset");
+                    this.TitleMetaDataOffset = this.PadValue(this.TicketOffset + this.TicketSize, "Title Meta Data Offset");
+                    this.DataOffset = this.PadValue(this.TitleMetaDataOffset + this.TitleMetaDataSize, "Data Offset");
+                    this.FooterOffset = this.PadValue(this.DataOffset + this.DataSize, "Footer Offset");
+
 
                 } // using (FileStream fs = File.OpenRead(sourceFile))
             }
@@ -63,23 +72,39 @@ namespace VGMToolbox.format
             }
         }
 
+        /// <summary>
+        /// Extracts all data from the WAD object.
+        /// </summary>
+        /// <returns>Array containing the paths of all extracted files.</returns>
         public string[] ExtractAll()
         {
             ArrayList extractedFiles = new ArrayList();
+            string footerFile;
 
-            extractedFiles.Add(this.ExtractCert());
+            extractedFiles.Add(this.ExtractCertificateChain());
+            extractedFiles.Add(this.ExtractTicket());
+            extractedFiles.Add(this.ExtractTitleMetaData());
 
-            
-            
+            // optional footer
+            footerFile = this.ExtractFooter();
+
+            if (!String.IsNullOrEmpty(footerFile))
+            {
+                extractedFiles.Add(footerFile);
+            }
             
             
             return (string[])extractedFiles.ToArray(typeof(string));
         }
 
-        public string ExtractCert()
+        /// <summary>
+        /// Extract the certificate chain.
+        /// </summary>
+        /// <returns>File name of extracted file.</returns>
+        public string ExtractCertificateChain()
         {
             string destinationFile = Path.Combine(
-                                        Path.Combine(Path.GetDirectoryName(this.SourceFileName), NintendoWad.EXTRACTION_FOLDER),
+                                        this.GetUnpackFolder(),
                                         (this.TitleId.ToString("X8") + ".cert"));
             
 
@@ -91,10 +116,73 @@ namespace VGMToolbox.format
             return destinationFile;
         }
 
+        /// <summary>
+        /// Extract the ticket.
+        /// </summary>
+        /// <returns>File name of extracted file.</returns>
+        public string ExtractTicket()
+        {
+            string destinationFile = Path.Combine(
+                                        this.GetUnpackFolder(),
+                                        (this.TitleId.ToString("X8") + ".tik"));
 
 
+            using (FileStream fs = File.OpenRead(this.SourceFileName))
+            {
+                ParseFile.ExtractChunkToFile(fs, this.TicketOffset, this.TicketSize, destinationFile);
+            }
+
+            return destinationFile;
+        }
+
+        /// <summary>
+        /// Extract the title meta data.
+        /// </summary>
+        /// <returns>File name of extracted file.</returns>
+        public string ExtractTitleMetaData()
+        {
+            string destinationFile = Path.Combine(
+                                        this.GetUnpackFolder(),
+                                        (this.TitleId.ToString("X8") + ".tmd"));
 
 
+            using (FileStream fs = File.OpenRead(this.SourceFileName))
+            {
+                ParseFile.ExtractChunkToFile(fs, this.TitleMetaDataOffset, this.TitleMetaDataSize, destinationFile);
+            }
+
+            return destinationFile;
+        }
+
+        /// <summary>
+        /// Extract optional footer.
+        /// </summary>
+        /// <returns>File name of extracted file or String.Empty if footer is not present.</returns>
+        public string ExtractFooter()
+        {
+            string destinationFile = String.Empty;
+
+            if (this.FooterSize > 0)
+            {
+                destinationFile = Path.Combine(
+                                    this.GetUnpackFolder(), 
+                                    (this.TitleId.ToString("X8") + ".trailer"));
+
+                using (FileStream fs = File.OpenRead(this.SourceFileName))
+                {
+                    ParseFile.ExtractChunkToFile(fs, this.FooterOffset, this.FooterSize, destinationFile);
+                }
+            }
+            
+            return destinationFile;
+        }
+
+        /// <summary>
+        /// Pads input value to next 0x40 byte alignment.
+        /// </summary>
+        /// <param name="value">Value to pad.</param>
+        /// <param name="textDescription">Text description of value used for error output.</param>
+        /// <returns>Value padded to next 0x40 byte alignment.</returns>
         private uint PadValue(uint value, string textDescription)
         {
             uint paddedValue;
@@ -114,6 +202,24 @@ namespace VGMToolbox.format
             return paddedValue;
         }
 
+        /// <summary>
+        /// Builds the output folder path for unpacking.
+        /// </summary>
+        /// <returns>Full path of folder for unpacking.</returns>
+        private string GetUnpackFolder()
+        {
+            string unpackFolder = Path.Combine(
+                Path.GetDirectoryName(this.SourceFileName),
+                String.Format("{0}_{1}", NintendoWad.EXTRACTION_FOLDER, Path.GetFileNameWithoutExtension(this.SourceFileName)));
+
+            return unpackFolder;
+        }
+
+        /// <summary>
+        /// Checks for WAD file Magic Bytes.
+        /// </summary>
+        /// <param name="sourceFile">Full path to file to check.</param>
+        /// <returns>Boolean value indicating if input file has WAD magic bytes.</returns>
         public static bool IsWadFile(string sourceFile)
         {
             bool isWad = false;
