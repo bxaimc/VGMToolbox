c15eec1ef2680f54252aa1cc2a169f930d5e48e9
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2015-07-31 04:53:32 +0000

    [CRI ACB EXTRACTOR] ACB updates.  Add handling of external AWBs.

--- format/VGMToolbox/format/CriAcbFile.cs
+++ format/VGMToolbox/format/CriAcbFile.cs
@@ -18,7 +18,10 @@ namespace VGMToolbox.format
         public const byte WAVEFORM_ENCODE_TYPE_ATRAC3 = 8;
         public const byte WAVEFORM_ENCODE_TYPE_BCWAV = 9;
         public const byte WAVEFORM_ENCODE_TYPE_NINTENDO_DSP = 13;
-        
+
+        public const string AWB_FORMAT1 = "{0}_streamfiles.awb";
+        public const string AWB_FORMAT2 = "{0}.awb";
+
         protected enum AwbToExtract { Internal, External };
 
         public CriAcbFile(FileStream fs, long offset)
@@ -28,10 +31,16 @@ namespace VGMToolbox.format
             // @TODO: Make CueId field a parameter
             this.GetCueNameToWaveformMap(fs, true);
 
-            // parse internal AWB
-            if (this.InternalAwbFileOffset > 0)
+            // initialize internal AWB
+            if (this.InternalAwbFileSize > 0)
             {
                 this.InternalAwb = new CriAfs2Archive(fs, (long)this.InternalAwbFileOffset);
+            }
+
+            // initialize external AWB
+            if (this.StreamAwbAfs2HeaderSize > 0)
+            {
+                this.ExternalAwb = this.InitializeExternalAwbArchive();
             }
         }
 
@@ -57,6 +66,14 @@ namespace VGMToolbox.format
                 return (ulong)CriUtfTable.GetOffsetForUtfFieldForRow(this, 0, "AwbFile");                
             }
         
+        }
+        public ulong InternalAwbFileSize
+        {
+            get
+            {
+                return (ulong)CriUtfTable.GetSizeForUtfFieldForRow(this, 0, "AwbFile");
+            }
+
         }
 
         public ulong CueTableOffset
@@ -138,9 +155,27 @@ namespace VGMToolbox.format
             string awbExtractionFolder = Path.Combine(baseExtractionFolder, "awb");
         
             
-            this.ExtractInternalAwb(internalAwbExtractionFolder);
+            // this.ExtractInternalAwb(internalAwbExtractionFolder);
             
-            
+            // extract internal awb
+            if (this.InternalAwb != null)
+            {
+                if (this.ExternalAwb == null)
+                {
+                    // use cue names since no AWB exists
+                    this.ExtractAwbWithCueNames(internalAwbExtractionFolder, AwbToExtract.Internal);
+                }
+                else
+                {
+                    // use AFS2 IDs, since cue names will apply to external AWB (@TODO: Does a flag exist?)
+                    this.InternalAwb.ExtractAllRaw(internalAwbExtractionFolder);
+                }
+            }
+
+            if (this.ExternalAwb != null)
+            {
+                this.ExtractAwbWithCueNames(awbExtractionFolder, AwbToExtract.External);
+            }
         }
 
         
@@ -210,13 +245,14 @@ namespace VGMToolbox.format
                                 switch (referenceType)
                                 {
                                     case 2:
+                                    case 8:
                                         referenceCorrection = 6;
                                         break;
                                     case 3:
                                         referenceCorrection = 2;
                                         break;
                                     default:
-                                        throw new FormatException(String.Format("  Unexpected ReferenceType: '{0}' for CueIndex: '{1}.'  Pleae report to VGMToolbox thread at hcs64.com forums, see link in 'Other' menu item.", referenceType.Value.ToString("D"), cueIndex.Value.ToString("D")));
+                                        throw new FormatException(String.Format("  Unexpected ReferenceType: '{0}' for CueIndex: '{1}.'  Please report to VGMToolbox thread at hcs64.com forums, see link in 'Other' menu item.", referenceType.Value.ToString("D"), cueIndex.Value.ToString("D")));
                                         break;
                                 }
 
@@ -296,7 +332,6 @@ namespace VGMToolbox.format
             }        
         }
 
-
         protected void ExtractAwbWithCueNames(string destinationFolder, AwbToExtract whichAwb)
         {
             CriUtfTable waveformTableUtf;
@@ -354,7 +389,65 @@ namespace VGMToolbox.format
                 }
             }
         }
-        
+
+        protected CriAfs2Archive InitializeExternalAwbArchive()
+        {
+            CriAfs2Archive afs2 = null;
+
+            string awbDirectory;
+            string awbMask;
+            string acbBaseFileName;
+            string[] awbFiles;
+
+            byte[] awbMd5Calculated;
+
+            awbDirectory = Path.GetDirectoryName(this.SourceFile);
+
+            // try format 1
+            acbBaseFileName = Path.GetFileNameWithoutExtension(this.SourceFile);
+            awbMask = String.Format(AWB_FORMAT1, acbBaseFileName);
+            awbFiles = Directory.GetFiles(awbDirectory, awbMask, SearchOption.TopDirectoryOnly);
+
+            if (awbFiles.Length < 1)
+            {
+                // try format 2
+                awbMask = String.Format(AWB_FORMAT2, acbBaseFileName);
+                awbFiles = Directory.GetFiles(awbDirectory, awbMask, SearchOption.TopDirectoryOnly);
+            }
+
+            // file not found
+            if (awbFiles.Length < 1)
+            {
+                throw new FileNotFoundException(String.Format("Cannot find AWB file. Please verify corresponding AWB file is named '{0}' or '{1}'.",
+                    String.Format(AWB_FORMAT1, acbBaseFileName), String.Format(AWB_FORMAT2, acbBaseFileName)));
+            }
+
+            if (awbFiles.Length > 1)
+            {
+                throw new FileNotFoundException(String.Format("More than one matching AWB file for this ACB. Please verify only one AWB file is named '{1}' or '{2}'.",
+                    String.Format(AWB_FORMAT1, acbBaseFileName), String.Format(AWB_FORMAT2, acbBaseFileName)));
+            }
+
+            // initialize AFS2 file                        
+            using (FileStream fs = File.Open(awbFiles[0], FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                // validate MD5 checksum
+                awbMd5Calculated = ByteConversion.GetBytesFromHexString(ChecksumUtil.GetMd5OfFullFile(fs));
+
+                if (ParseFile.CompareSegment(awbMd5Calculated, 0, this.StreamAwbHash))
+                {
+                    afs2 = new CriAfs2Archive(fs, 0);
+                }
+                else
+                {
+                    throw new FormatException(String.Format("AWB file, <{0}>, did not match MD5 checksum inside ACB file.", Path.GetFileName(fs.Name)));
+                }
+            }
+
+            return afs2;
+        }
+
+
         public static string GetFileExtensionForEncodeType(byte encodeType)
         {
             string ext;
@@ -383,5 +476,7 @@ namespace VGMToolbox.format
 
             return ext;
         }
+
+
     }
 }
