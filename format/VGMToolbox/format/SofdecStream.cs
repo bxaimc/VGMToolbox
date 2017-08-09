020547455de367e002428474ccedb62ee108e0b3
snakemeat <snakemeat@a691631e-124d-0410-8249-85b9adaa1b1e>
2017-08-09 04:11:04 +0000

    [VIDEO DEMUXER] Add file extension for .AC3 for SFD files.

--- format/VGMToolbox/format/SofdecStream.cs
+++ format/VGMToolbox/format/SofdecStream.cs
@@ -10,8 +10,10 @@ namespace VGMToolbox.format
 
         public const string AdxAudioExtension = ".adx";
         public const string AixAudioExtension = ".aix";
+        public const string Ac3AudioExtension = ".ac3";
 
         public static readonly byte[] AixSignatureBytes = new byte[] { 0x41, 0x49, 0x58, 0x46 };
+        public static readonly byte[] Ac3SignatureBytes = new byte[] { 0x0B, 0x77 };
 
         public SofdecStream(string path): base(path) 
         {
@@ -22,22 +24,31 @@ namespace VGMToolbox.format
         protected override string GetAudioFileExtension(Stream readStream, long currentOffset)
         {
             string fileExtension;
-            byte[] checkBytes;
+            byte[] checkBytes, checkBytesAc3;
 
             int headerSize = this.GetAudioPacketHeaderSize(readStream, currentOffset);
             checkBytes = ParseFile.ParseSimpleOffset(readStream, (currentOffset + 6 + headerSize), 4);
-
+            
             if (ParseFile.CompareSegment(checkBytes, 0, AixSignatureBytes))
             {
                 fileExtension = AixAudioExtension;
-            }
+            }            
             else if (checkBytes[0] == 0x80)
             {
                 fileExtension = AdxAudioExtension;
             }
             else
             {
-                fileExtension = ".bin";
+                checkBytesAc3 = ParseFile.ParseSimpleOffset(readStream, (currentOffset + 6 + headerSize), 2);
+
+                if (ParseFile.CompareSegment(checkBytesAc3, 0, Ac3SignatureBytes))
+                {
+                    fileExtension = Ac3AudioExtension;
+                }
+                else
+                {
+                    fileExtension = ".bin";
+                }
             }
 
             return fileExtension;
